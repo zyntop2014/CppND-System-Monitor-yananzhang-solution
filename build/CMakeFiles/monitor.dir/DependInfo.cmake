245e23ba6220caa645ca8de6225b1798da690b87
yanan <zyntop2014@gmail.com>
2020-08-24 22:13:27 -0400

    add starter files

---
+++ build/CMakeFiles/monitor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/src/format.cpp" "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/build/CMakeFiles/monitor.dir/src/format.cpp.o"
+  "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/src/linux_parser.cpp" "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/build/CMakeFiles/monitor.dir/src/linux_parser.cpp.o"
+  "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/src/main.cpp" "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/build/CMakeFiles/monitor.dir/src/main.cpp.o"
+  "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/src/ncurses_display.cpp" "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/build/CMakeFiles/monitor.dir/src/ncurses_display.cpp.o"
+  "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/src/process.cpp" "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/build/CMakeFiles/monitor.dir/src/process.cpp.o"
+  "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/src/processor.cpp" "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/build/CMakeFiles/monitor.dir/src/processor.cpp.o"
+  "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/src/system.cpp" "/home/yananzhang/workspace/CppND-System-Monitor-yananzhang-solution/build/CMakeFiles/monitor.dir/src/system.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
